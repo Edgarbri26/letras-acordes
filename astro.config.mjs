42a8351d20878bd91ee05117d5f4c11da56c1ece
Edgarbriceno26@outlook.es <Edgarbriceno26@outlook.es>
2025-12-29 23:48:25 -0400

    prueba

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,16 +12,12 @@ import vercel from '@astrojs/vercel';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
 
-  
+
 
 
   integrations: [react()]
